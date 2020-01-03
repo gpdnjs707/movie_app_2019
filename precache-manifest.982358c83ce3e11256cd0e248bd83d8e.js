9b82756f5023602d58ed4bd11d163f9269a66c12
Hyewon Jeong <hyewon@Hyewons-MacBook-Pro.local>
2020-01-03 13:18:58 +0900

    Updates

---
+++ precache-manifest.982358c83ce3e11256cd0e248bd83d8e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e798c4ff189d9504d2e453d7d773e679",
+    "url": "/movie_app_2019/index.html"
+  },
+  {
+    "revision": "f4d951a6b40dd8f3ae4e",
+    "url": "/movie_app_2019/static/css/main.b0cd69dd.chunk.css"
+  },
+  {
+    "revision": "8526568d949b0ffebe66",
+    "url": "/movie_app_2019/static/js/2.a187f9e2.chunk.js"
+  },
+  {
+    "revision": "c6d01cb19213691bc81a274434d00fe7",
+    "url": "/movie_app_2019/static/js/2.a187f9e2.chunk.js.LICENSE"
+  },
+  {
+    "revision": "f4d951a6b40dd8f3ae4e",
+    "url": "/movie_app_2019/static/js/main.22af29ae.chunk.js"
+  },
+  {
+    "revision": "101d7acd549509bcf93b",
+    "url": "/movie_app_2019/static/js/runtime-main.69d003e7.js"
+  }
+]);
